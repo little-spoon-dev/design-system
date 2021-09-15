1c4643e1deabfe8b4da42cf194df6bba980324b5
Mark Xu <mark.xu@littlespoon.com>
2021-09-15 13:04:52 -0400

    feat(storybook): add stories for theme colors LS-1711

---
+++ packages/storybook/src/stories/Colors.stories.tsx
@@ -0,0 +1,33 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import Colors from './Colors'
+import theme from '@littlespoon/theme'
+
+export default {
+  title: 'Example/Theme',
+  component: Colors,
+} as ComponentMeta<typeof Colors>
+
+const Template: ComponentStory<typeof Colors> = (args) => <Colors {...args} />
+
+export const Token = Template.bind({})
+Token.args = {
+  color: {
+    token: theme.colors.token,
+  },
+}
+
+export const Primary = Template.bind({})
+Primary.args = {
+  color: theme.colors.primary,
+}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+  color: theme.colors.secondary,
+}
+
+export const Alert = Template.bind({})
+Alert.args = {
+  color: theme.colors.alert,
+}

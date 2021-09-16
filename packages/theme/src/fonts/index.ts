1594f5452fa282082ca08a9d2441146951d34f45
Ian <ian.michael.garrett@gmail.com>
2021-09-16 16:59:52 -0400

    fix(theme): update snapshot, export fonts

--- packages/theme/src/fonts/index.ts
+++ packages/theme/src/fonts/index.ts
@@ -1,19 +1,18 @@
-export const fonts = {
-  primary: {
-    family: 'Lato, sans-serif',
-    weight: {
-      normal: 400,
-      bold: 700,
-    },
-  },
-  secondary: {
-    family: 'Mulish, sans-serif',
-    weight: {
-      bold: 700,
-      extraBold: 800,
-      black: 900,
-    },
+export const primary = {
+  family: 'Lato, sans-serif',
+  weight: {
+    normal: 400,
+    bold: 700,
   },
 }
 
-export default fonts
+export const secondary = {
+  family: 'Mulish, sans-serif',
+  weight: {
+    bold: 700,
+    extraBold: 800,
+    black: 900,
+  },
+}
+
+export default { primary, secondary }

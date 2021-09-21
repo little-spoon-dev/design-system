614c60c693b164322e89ac7a7e645c63da5d7e52
Ian <ian.michael.garrett@gmail.com>
2021-09-21 10:02:01 -0400

    refactor(theme): update font sizes to 10px scale

--- packages/theme/src/fonts/index.ts
+++ packages/theme/src/fonts/index.ts
@@ -1,15 +1,20 @@
+/**
+ * Fonts
+ *
+ * {@link https://zeroheight.com/3ddd0f892/p/211297-typography/b/655ccf}
+ */
 export const primary = {
   family: 'Lato, sans-serif',
   variant: {
-    p1: '1.25rem',
-    p2: '1.125rem',
-    p3: '1rem',
-    p4: '0.875rem',
-    caption1: '0.75rem',
-    button1: '1.25rem',
-    button2: '1.125rem',
-    button3: '1rem',
-    button4: '0.875rem',
+    p1: '2rem',
+    p2: '1.8rem',
+    p3: '1.6rem',
+    p4: '1.4rem',
+    caption1: '1.2rem',
+    button1: '2rem',
+    button2: '1.8rem',
+    button3: '1.6rem',
+    button4: '1.4rem',
   },
   weight: {
     normal: 400,
@@ -20,14 +25,14 @@ export const primary = {
 export const secondary = {
   family: 'Mulish, sans-serif',
   variant: {
-    display1: '4.625rem',
-    display2: '4.125rem',
-    h1: '3.25rem',
-    h2: '2.875rem',
-    h3: '2.25rem',
-    h4: '1.8125rem',
-    h5: '1.625rem',
-    h6: '1.25rem',
+    display1: '7.4rem',
+    display2: '6.6rem',
+    h1: '5.2rem',
+    h2: '4.6rem',
+    h3: '3.6rem',
+    h4: '2.9rem',
+    h5: '2.6rem',
+    h6: '2rem',
   },
   weight: {
     bold: 700,

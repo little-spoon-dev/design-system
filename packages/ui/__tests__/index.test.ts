3c6620890ecb58dfe5ae721ad7573634f0b12cdd
Mark Xu <mark.xu@littlespoon.com>
2021-10-13 20:17:15 -0400

    feat(ui): export Typography LS-1883

--- packages/ui/__tests__/index.test.ts
+++ packages/ui/__tests__/index.test.ts
@@ -1,20 +1,47 @@
+import { Breadcrumb, Breadcrumbs } from '@littlespoon/breadcrumbs'
 import Button from '@littlespoon/button'
+import Divider from '@littlespoon/divider'
 import icons from '@littlespoon/icons'
 import theme from '@littlespoon/theme'
+import Typography from '@littlespoon/typography'
 import { createElement, isValidElement } from 'react'
 
 import ui from '..'
+import uiBreadcrumb from '../Breadcrumb'
+import uiBreadcrumbs from '../Breadcrumbs'
 import uiButton from '../Button'
+import uiDivider from '../Divider'
 import uiIcons from '../icons'
 import uiTheme from '../theme'
+import uiTypography from '../Typography'
 
 it('exports ui', () => {
   expect(ui).toMatchSnapshot()
 })
 
+it('exports Breadcrumb', () => {
+  expect(uiBreadcrumb).toBe(Breadcrumb)
+  expect(isValidElement(createElement(Breadcrumb))).toBe(true)
+})
+
+it('exports Breadcrumbs', () => {
+  expect(uiBreadcrumbs).toBe(Breadcrumbs)
+  expect(isValidElement(createElement(Breadcrumbs))).toBe(true)
+})
+
 it('exports Button', () => {
   expect(uiButton).toBe(Button)
   expect(isValidElement(createElement(Button))).toBe(true)
+})
+
+it('exports Divider', () => {
+  expect(uiDivider).toBe(Divider)
+  expect(isValidElement(createElement(Divider))).toBe(true)
+})
+
+it('exports Typography', () => {
+  expect(uiTypography).toBe(Typography)
+  expect(isValidElement(createElement(Typography))).toBe(true)
 })
 
 it('exports theme', () => {
--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -2,13 +2,15 @@ import { Breadcrumb, Breadcrumbs } from '@littlespoon/breadcrumbs'
 import Button from '@littlespoon/button'
 import Divider from '@littlespoon/divider'
 import theme from '@littlespoon/theme'
+import Typography from '@littlespoon/typography'
 
-export { Breadcrumb, Breadcrumbs, Button, Divider, theme }
+export { Breadcrumb, Breadcrumbs, Button, Divider, theme, Typography }
 
 export default {
   Breadcrumb,
   Breadcrumbs,
   Button,
   Divider,
+  Typography,
   theme,
 }

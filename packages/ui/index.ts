950b73df6fbd508a1f13b4468e831335479377de
Ian <ian.michael.garrett@gmail.com>
2021-10-14 16:16:22 -0400

    chore(checkbox): add CheckboxGroup export to ui

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,17 +1,18 @@
 import { Breadcrumb, Breadcrumbs } from '@littlespoon/breadcrumbs'
 import Button from '@littlespoon/button'
-import { Checkbox } from '@littlespoon/checkbox'
+import { Checkbox, CheckboxGroup } from '@littlespoon/checkbox'
 import Divider from '@littlespoon/divider'
 import theme from '@littlespoon/theme'
 import Typography from '@littlespoon/typography'
 
-export { Breadcrumb, Breadcrumbs, Button, Checkbox, Divider, theme, Typography }
+export { Breadcrumb, Breadcrumbs, Button, Checkbox, CheckboxGroup, Divider, theme, Typography }
 
 export default {
   Breadcrumb,
   Breadcrumbs,
   Button,
   Checkbox,
+  CheckboxGroup,
   Divider,
   Typography,
   theme,

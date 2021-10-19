513b43d55b7c3ce9cbb0c428239a1e9f1b2a7ce1
Mark Xu <mark.xu@littlespoon.com>
2021-10-19 12:48:16 -0400

    feat(icons): add props fill and stroke to CloseIcon LS-2041

--- packages/icons/src/CloseIcon.tsx
+++ packages/icons/src/CloseIcon.tsx
@@ -1,14 +1,33 @@
 import { shadeBlack, shadeWhite } from '@littlespoon/theme/lib/colors/token'
 
-export default function CloseIcon() {
+export interface CloseIconProps extends React.SVGAttributes<SVGElement> {
+  /**
+   * The fill of the component. Defaults to white.
+   */
+  fill?: string
+
+  /**
+   * The stroke of the component. Defaults to black.
+   */
+  stroke?: string
+}
+
+export default function CloseIcon({ fill = shadeWhite, stroke = shadeBlack, ...other }) {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="12" cy="12" r="12" fill={shadeWhite} />
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...other}
+    >
+      <circle cx="12" cy="12" r="12" fill={fill} />
       <title>Close icon</title>
       <desc>Close icon</desc>
       <path
         d="M7.56567 7.56583L16.4342 16.4343M7.56568 16.4343L16.4342 7.56583"
-        stroke={shadeBlack}
+        stroke={stroke}
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"

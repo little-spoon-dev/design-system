9b2f7db1a9e9c360ea94d215ae2d001767bd8607
bnikom <belka.nikom@gmail.com>
2021-10-20 14:14:54 -0400

    fix: rotate

--- packages/icons/src/OpenIcon.tsx
+++ packages/icons/src/OpenIcon.tsx
@@ -20,6 +20,7 @@ export default function OpenIcon({ fill = shadeWhite, stroke = shadeBlack, ...ot
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      transform="rotate(45)"
       {...other}
     >
       <circle cx="12" cy="12" r="12" fill={fill} />

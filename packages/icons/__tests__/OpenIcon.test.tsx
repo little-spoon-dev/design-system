1c3bdbcdf8a9cc84db8c644deab83053b20f61da
bnikom <belka.nikom@gmail.com>
2021-10-20 14:46:24 -0400

    fix(icons): name in test

--- packages/icons/__tests__/OpenIcon.test.tsx
+++ packages/icons/__tests__/OpenIcon.test.tsx
@@ -1,40 +1,40 @@
 import { render, screen } from '@testing-library/react'
 import { axe } from 'jest-axe'
 
-import CloseIcon from '../src/CloseIcon'
+import OpenIcon from '../src/OpenIcon'
 
 const label = 'label'
 
 describe('accessibility', () => {
   it('is accessible with no props', async () => {
-    const { container } = render(<CloseIcon />)
+    const { container } = render(<OpenIcon />)
     expect(await axe(container)).toHaveNoViolations()
   })
 
   it('has title', async () => {
-    render(<CloseIcon />)
-    expect(screen.getByTitle('Close icon')).toBeInTheDocument()
+    render(<OpenIcon />)
+    expect(screen.getByTitle('Open icon')).toBeInTheDocument()
   })
 })
 
 describe('with props.className', () => {
   it('renders filter icon with className', () => {
     const className = 'class'
-    render(<CloseIcon aria-label={label} className={className} />)
+    render(<OpenIcon aria-label={label} className={className} />)
     expect(screen.getByLabelText(label)).toHaveClass(className)
   })
 })
 
 describe('with props.fill', () => {
   it('renders filter icon with stroke', () => {
-    render(<CloseIcon fill="#bada55" />)
-    expect(screen.getByTitle('Close icon')).toBeInTheDocument()
+    render(<OpenIcon fill="#bada55" />)
+    expect(screen.getByTitle('Open icon')).toBeInTheDocument()
   })
 })
 
 describe('with props.stroke', () => {
   it('renders filter icon with stroke', () => {
-    render(<CloseIcon stroke="#bada55" />)
-    expect(screen.getByTitle('Close icon')).toBeInTheDocument()
+    render(<OpenIcon stroke="#bada55" />)
+    expect(screen.getByTitle('Open icon')).toBeInTheDocument()
   })
 })

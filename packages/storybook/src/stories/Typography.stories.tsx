95c59aaa2db077b85cf553bf7d0efcad47702868
Mark Xu <mark.xu@littlespoon.com>
2021-10-20 19:46:51 -0400

    feat(storybook): update Typography stories LS-2016

--- packages/storybook/src/stories/Typography.stories.tsx
+++ packages/storybook/src/stories/Typography.stories.tsx
@@ -8,6 +8,50 @@ export default {
 
 const Template: ComponentStory<typeof Typography> = (args) => <Typography {...args} />
 
+const TemplateAll: ComponentStory<typeof Typography> = (args) => (
+  <>
+    <Typography as="p" {...args}>
+      Paragraph
+    </Typography>
+
+    <Typography variant="caption1" {...args}>
+      Caption 1
+    </Typography>
+
+    <Typography as="h1" variant="display1" {...args}>
+      Display 1
+    </Typography>
+
+    <Typography as="h2" variant="display2" {...args}>
+      Display 2
+    </Typography>
+
+    <Typography as="h1" {...args}>
+      Heading 1
+    </Typography>
+
+    <Typography as="h2" {...args}>
+      Heading 2
+    </Typography>
+
+    <Typography as="h3" {...args}>
+      Heading 3
+    </Typography>
+
+    <Typography as="h4" {...args}>
+      Heading 4
+    </Typography>
+
+    <Typography as="h5" {...args}>
+      Heading 5
+    </Typography>
+
+    <Typography as="h6" {...args}>
+      Heading 6
+    </Typography>
+  </>
+)
+
 export const Default = Template.bind({})
 Default.args = {
   children: 'Defaults to paragraph.',
@@ -17,6 +61,24 @@ export const Paragraph = Template.bind({})
 Paragraph.args = {
   as: 'p',
   children: 'Paragraph',
+}
+
+export const Caption1 = Template.bind({})
+Caption1.args = {
+  variant: 'caption1',
+  children: 'Caption 1',
+}
+
+export const Display1 = Template.bind({})
+Display1.args = {
+  children: 'Display 1',
+  variant: 'display1',
+}
+
+export const Display2 = Template.bind({})
+Display2.args = {
+  children: 'Display 2',
+  variant: 'display2',
 }
 
 export const Heading1 = Template.bind({})
@@ -55,12 +117,6 @@ Heading6.args = {
   children: 'Heading 6',
 }
 
-export const NoMargin = Template.bind({})
-NoMargin.args = {
-  children: 'No margin',
-  noMargin: true,
-}
-
 export const Variant = Template.bind({})
 Variant.args = {
   as: 'h1',
@@ -68,14 +124,27 @@ Variant.args = {
   variant: 'h6',
 }
 
-const Template2: ComponentStory<typeof Typography> = (args) => (
-  <>
-    <Typography {...args} />
-    <Typography {...args} />
-  </>
-)
+export const NoMargin = TemplateAll.bind({})
+NoMargin.args = {
+  noMargin: true,
+}
 
-export const Multiple = Template2.bind({})
-Multiple.args = {
-  children: 'Lorem ipsum dolor sit amet.',
+export const Bold = TemplateAll.bind({})
+Bold.args = {
+  bold: true,
+}
+
+export const ExtraBold = TemplateAll.bind({})
+ExtraBold.args = {
+  extraBold: true,
+}
+
+export const Black = TemplateAll.bind({})
+Black.args = {
+  black: true,
+}
+
+export const Uppercase = TemplateAll.bind({})
+Uppercase.args = {
+  uppercase: true,
 }

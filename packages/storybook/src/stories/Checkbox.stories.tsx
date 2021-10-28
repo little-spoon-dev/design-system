4b5dff1edc7edcc1ba36993ea7142455bd7f12c9
fjallkraken <fjallkraken@gmail.com>
2021-10-27 21:43:11 -0500

    fix(checkbox): updates stories to have internal state

--- packages/storybook/src/stories/Checkbox.stories.tsx
+++ packages/storybook/src/stories/Checkbox.stories.tsx
@@ -1,34 +1,63 @@
 import { Checkbox, CheckboxGroup } from '@littlespoon/checkbox/src/Checkbox'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
+import React from 'react'
 
 export default {
   title: 'Design System/Checkbox',
   component: Checkbox,
+  argTypes: {
+    checked: {
+      control: false,
+    },
+  },
 } as ComponentMeta<typeof Checkbox>
 
-const CheckboxTemplate: ComponentStory<typeof Checkbox> = (args) => <Checkbox {...args} />
+// Checkbox
+
+const CheckboxTemplate: ComponentStory<typeof Checkbox> = (args) => {
+  const [checked, setChecked] = React.useState(false)
+  const toggleChecked = () => setChecked(!checked)
+
+  return <Checkbox {...args} onChange={toggleChecked} checked={checked} />
+}
 
 export const Default = CheckboxTemplate.bind({})
-Default.args = {
-  children: 'Text',
-}
+Default.args = { children: 'Text' }
 
 export const Disabled = CheckboxTemplate.bind({})
-Disabled.args = {
-  children: 'Text',
-  disabled: true,
-}
+Disabled.args = { children: 'Text', disabled: true }
 
-const checkboxProps = {
-  children: 'Text',
-}
+// Checkbox Group
 
-const CheckboxGroupTemplate: ComponentStory<typeof CheckboxGroup> = (args) => (
-  <CheckboxGroup {...args}>
-    <Checkbox {...checkboxProps} /> <Checkbox {...checkboxProps} /> <Checkbox {...checkboxProps} />
-    <Checkbox {...checkboxProps} />
-  </CheckboxGroup>
-)
+const CheckboxGroupTemplate: ComponentStory<typeof CheckboxGroup> = (args) => {
+  const [checkedGroup, setChecked] = React.useState([false, false, false, false])
+  const toggleChecked = (indexToToggle: number) => {
+    setChecked(
+      checkedGroup.map((value, index) => {
+        if (index === indexToToggle) {
+          return !value
+        }
+        return value
+      }),
+    )
+  }
+
+  return (
+    <CheckboxGroup {...args}>
+      {checkedGroup.map((checked, index) => (
+        <Checkbox
+          key={index}
+          checked={checked}
+          onChange={() => {
+            toggleChecked(index)
+          }}
+        >
+          {args.children || 'Text'} - {index}
+        </Checkbox>
+      ))}
+    </CheckboxGroup>
+  )
+}
 
 export const CheckboxGroupVertical = CheckboxGroupTemplate.bind({})
 CheckboxGroupVertical.args = {}

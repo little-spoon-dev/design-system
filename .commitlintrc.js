c3609dcc3a82917e28e460a3959130bdcd706c12
Mark Xu <mark.xu@littlespoon.com>
2022-06-22 10:55:12 -0400

    chore(root): warn for commitlint rule footer-max-line-length

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [1, 'always', 100],
+    'footer-max-line-length': [1, 'always', 80],
     'header-max-length': [1, 'always', 72],
     'scope-enum': [
       2,

d0c7441aa182dfcc559cf7a3734afa3e3118c3d1
Andrii Matviichuk <matviichuk.andr@gmail.com>
2022-08-30 18:31:37 +0200

    feat(icons): add utils/css-helpers for getting CSS props LS-3941

---
+++ packages/icons/src/utils/css-helpers.ts
@@ -0,0 +1,55 @@
+import { rem } from '@littlespoon/theme/lib/utils'
+
+/**
+ * Returns rotation value for given direction.
+ * Works only if icon initially points down
+ */
+export function getRotate(direction: string | undefined): number {
+  switch (direction) {
+    case 'up':
+      return 180
+    case 'left':
+      return 90
+    case 'right':
+      return -90
+    case 'down':
+    default:
+      return 0
+  }
+}
+
+/**
+ * Returns the scale property based on the size (xsmall, small, medium, large)
+ * Defaults to 1 (small)
+ */
+export function getScale(size: string | undefined): number {
+  switch (size) {
+    case 'xsmall':
+      return 0.5
+    case 'medium':
+      return 2
+    case 'large':
+      return 3.2
+    case 'small':
+    default:
+      return 1
+  }
+}
+
+/**
+ * Returns the size in rems based on the size (xsmall, small, medium, large)
+ * Base size can be passed, default is 4.4
+ */
+export function getSvgSize(size: string | undefined, baseSize = 4.4): string {
+  switch (size) {
+    case 'xsmall':
+      return rem(baseSize * 0.5)
+    case 'medium':
+      return rem(baseSize * 2)
+    case 'large':
+      return rem(baseSize * 3.2)
+    case 'small':
+    default:
+      return rem(baseSize)
+  }
+}

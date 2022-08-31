3f9ac8c454cf56250f080fc159c56107b2d7305b
Andrii Matviichuk <matviichuk.andr@gmail.com>
2022-09-01 00:46:01 +0200

    feat(icons): add size prop to close icon LS-3568

--- packages/storybook/src/stories/icons/CloseIcon.stories.tsx
+++ packages/storybook/src/stories/icons/CloseIcon.stories.tsx
@@ -11,6 +11,26 @@ const Template: ComponentStory<typeof CloseIcon> = (args) => <CloseIcon {...args
 export const Default = Template.bind({})
 Default.args = {}
 
+export const ExtraSmall = Template.bind({})
+ExtraSmall.args = {
+  size: 'xsmall',
+}
+
+export const Small = Template.bind({})
+Small.args = {
+  size: 'small',
+}
+
+export const Medium = Template.bind({})
+Medium.args = {
+  size: 'medium',
+}
+
+export const Large = Template.bind({})
+Large.args = {
+  size: 'large',
+}
+
 export const Fill = Template.bind({})
 Fill.args = {
   fill: 'skyblue',

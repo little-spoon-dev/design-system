a7da564628461d6b317d12cfef2e7a1dd41d7446
Ryan Diercks <ryanjdiercks@gmail.com>
2022-09-06 12:15:42 -0400

    fix(accordion): fix testing failure

--- packages/icons/__tests__/AddIcon.test.tsx
+++ packages/icons/__tests__/AddIcon.test.tsx
@@ -23,7 +23,9 @@ describe('with props.size', () => {
     'renders bill icon with size=%j',
     (size) => {
       render(<AddIcon aria-label={label} size={size} />)
-      expect(screen.getByLabelText(label)).toHaveStyle(`transform: scale(${getScale(size)})`)
+      expect(screen.getByLabelText(label)).toHaveStyle(
+        `transform: rotate(45deg) scale(${getScale(size)})`,
+      )
     },
   )
 

33a62dbb70cf730d8e9b755eb7ab63182026955f
Ryan Diercks <ryanjdiercks@gmail.com>
2022-10-26 12:14:58 -0400

    fix(alert): fix alert ui issues

--- packages/alert/src/AlertBase.tsx
+++ packages/alert/src/AlertBase.tsx
@@ -1,7 +1,8 @@
 import Button from '@littlespoon/button'
 import Link from '@littlespoon/link'
+import breakpoints from '@littlespoon/theme/lib/breakpoints'
 import colors from '@littlespoon/theme/lib/colors'
-import { p3, p4 } from '@littlespoon/theme/lib/fonts/paragraph'
+import { p2, p3, p4 } from '@littlespoon/theme/lib/fonts/paragraph'
 import { family, weight } from '@littlespoon/theme/lib/fonts/primary'
 import { rem } from '@littlespoon/theme/lib/utils'
 import styled, { Keyframes, keyframes } from 'styled-components'
@@ -33,7 +34,7 @@ const fadeoutTop = keyframes`
 export const AlertWrapper = styled.div<BaseAlertProps>`
   display: flex;
   position: ${(props) => (props.type === AlertTypes.RELATIVE ? 'relative' : 'fixed')};
-  border-radius: ${rem(0.2)};
+  border-radius: ${rem(0.4)};
   border: 0;
   box-sizing: border-box;
   padding: ${rem(0.8)};
@@ -49,26 +50,41 @@ export const AlertWrapper = styled.div<BaseAlertProps>`
 `
 
 export const AlertMessages = styled.div<Partial<AlertProps>>`
-  margin-left: ${rem(0.8)};
+  margin-left: ${rem(1)};
   display: flex;
   flex-direction: column;
   justify-content: center;
 `
 
 export const AlertTitle = styled.span<Partial<AlertProps>>`
-  font: ${weight.bold} ${p3.fontSize} ${family};
-  line-height: ${p3.lineHeight};
+  font: ${weight.bold} ${p2.fontSize} ${family};
+  line-height: ${p2.lineHeight};
+
+  @media (max-width: ${breakpoints.sm}px) {
+    font: ${weight.bold} ${p3.fontSize} ${family};
+    line-height: ${p3.lineHeight};
+  }
 `
 
 export const AlertDescription = styled.span<Partial<AlertProps>>`
-  font: ${weight.normal} ${p4.fontSize} ${family};
-  line-height: ${p4.lineHeight};
+  font: ${weight.normal} ${p3.fontSize} ${family};
+  line-height: ${p3.lineHeight};
   vertical-align: middle;
+
+  @media (max-width: ${breakpoints.sm}px) {
+    font: ${weight.normal} ${p4.fontSize} ${family};
+    line-height: ${p4.lineHeight};
+  }
 `
 
 export const AlertActionLink = styled(Link)<Partial<AlertProps>>`
-  line-height: ${p4.lineHeight};
-  font: ${weight.bold} ${p4.fontSize} ${family};
+  line-height: ${p3.lineHeight};
+  font: ${weight.bold} ${p3.fontSize} ${family};
+
+  @media (max-width: ${breakpoints.sm}px) {
+    line-height: ${p4.lineHeight};
+    font: ${weight.bold} ${p4.fontSize} ${family};
+  }
 `
 
 export const VisuallyHidden = styled.span`
